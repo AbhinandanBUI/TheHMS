217472a80748123427450dbd3d5c30e5c175df2e
Tanishk Singh <abhinandansinghtt08@gmail.com>
2025-08-15 17:56:59 +0530

    project starter

--- TheHMS.AppHost/Program.cs
+++ TheHMS.AppHost/Program.cs
@@ -1,14 +1,27 @@
+using System.Diagnostics;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 //var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.TheHMS_ApiService>("apiservice");
- 
+
+var process = new Process
+{
+    StartInfo = new ProcessStartInfo
+    {
+        FileName = "cmd.exe",
+        Arguments = "/c npm start", // or ng serve
+        WorkingDirectory = @"D:\Client\TheHMS\TheHMS.Web\",
+        UseShellExecute = false
+    }
+};
+
+process.Start();
+
 var webFrontend = builder.AddProject<Projects.TheHMS_Web>("webfrontend")
     .WithExternalHttpEndpoints()
-    .WithReference(apiService)
-    ;
- 
+    .WithReference(apiService);
 
 
 //builder.AddProject<Projects.TheHMS_Web>("webfrontend")
